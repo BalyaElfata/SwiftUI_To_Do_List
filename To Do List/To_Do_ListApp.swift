804851367ce2770903298e3e3e8c08f9d6f915e0
Balya Elfata <balyaelfata@yahoo.com>
2024-07-07 17:48:16 +0700

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Balya Elfata on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
