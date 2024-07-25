fc8d0345971f4a33bbae6f6d3e5e88c7dc49401e
Balya Elfata <balyaelfata@yahoo.com>
2024-07-25 14:05:47 +0700

    To Do List App initial commit

--- To Do List/Other/To_Do_ListApp.swift
+++ To Do List/Other/To_Do_ListApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct To_Do_ListApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
