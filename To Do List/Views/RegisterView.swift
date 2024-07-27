160abc2187709012aa9cd6fc3c712ef6e27d123c
Balya Elfata <balyaelfata@yahoo.com>
2024-07-27 20:20:29 +0700

    added register view and viewmodel

---
+++ To Do List/Views/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  To Do List
+//
+//  Created by Balya Elfata on 27/07/24.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
