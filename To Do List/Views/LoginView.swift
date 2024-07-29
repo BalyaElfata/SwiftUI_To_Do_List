df5ac2d1b5ae8adc8b889421360d46e2c9a58978
Balya Elfata <balyaelfata@yahoo.com>
2024-07-29 13:22:29 +0700

    added login form UI

--- To Do List/Views/LoginView.swift
+++ To Do List/Views/LoginView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
             // Header
             HeaderView()
             
             // Login Form
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button {
+                    // attempt log in
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerSize: 10)
+                            .foregroundStyle(Color.blue)
+                        
+                        Text("Log In")
+                            .foregroundStyle(Color.white)
+                            .bold()
+                    }
+                }
+            }
             
             // Create Account
             
